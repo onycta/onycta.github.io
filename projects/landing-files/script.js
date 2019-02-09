dcbd8caf8550e40add889245cb2e41afd35dae53
Onycta <raven-loo@yandex.ru>
2019-02-09 17:09:08 +0300

    Fixed some bugs with links. Also moved 'greybite' files to parent folder. Renamed this folder accordingly.

--- projects/landing-files/script.js
+++
@@ -1,9 +0,0 @@
-menu.onclick = function showMenu(){
-	var x = document.getElementById("topnav");
-	
-	if (x.className == "topnav") {
-		x.className += " responsive";
-	} else {
-		x.className = 'topnav';
-	}
-}
